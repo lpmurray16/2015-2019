02952bb0231bd41b819adfc23eb1ddd337da4e0e
Logan Murray <43253119+lpmurray16@users.noreply.github.com>
2019-05-21 14:53:21 -0500

    Add files via upload

---
+++ Scheme_hwks/simple.scm
@@ -0,0 +1,112 @@
+; Logan Murray
+; 2019-01-17
+; CSC 347-501
+
+
+; INSTRUCTIONS
+;
+; Type in all of the expressions (a)-(o) from Exercise 2.2.3 of 
+; http://www.scheme.com/tspl4/start.html   
+; For each expression, type in its value beneath the expression (as a comment).  
+; Use https://repl.it/languages/Scheme/ or SISC Scheme to verify your answers (see worksheet 1).
+; (a) has been completed for you.
+; 
+; SUBMISSION
+;
+; 1. Push your local repository to the repository created for you on Bitbucket before the deadline.
+;
+; 2. Late submissions will not be permitted because solutions will be discussed in class.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(a)
+
+(cons 'car 'cdr)
+; evaluates to: (car . cdr)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(b)
+
+; evaluates to: (this (is silly))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(c)
+
+; evaluates to: (is this silly?)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(d)
+
+; evaluates to: (+ 2 3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(e)
+
+; evaluates to: (+ 2 3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(f)
+
+; evaluates to: +
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(g)
+
+; evaluates to: (2 3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(h)
+
+; evaluates to: <native procedure cons>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(i)
+
+; evaluates to: cons
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(j)
+
+; evaluates to: 'cons
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(k)
+
+; evaluates to: quote
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(l)
+
+; evaluates to: 5
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(m)
+
+; evaluates to: 5
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(n)
+
+; evaluates to: 5
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 2.2.3(o)
+
+; evaluates to: 5
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
